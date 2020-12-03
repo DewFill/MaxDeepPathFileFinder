5d04a3f0c4bc3f5d3367ebb4bfa648a09f7892f5
DewFill <24319994+DewFill@users.noreply.github.com>
2020-12-03 10:11:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.Arrays;
+
+public class Main {
+    int num = 0;
+    int numberOfFiles = 0;
+    int maxDeepNum = 0;
+    File maxDeepFile = new File("");
+
+    public static void main(String[] args) {
+        File basedirPath = new File("C:/Users/user/Desktop/basedir");
+        Main main = new Main();
+        main.findDeep(basedirPath);
+        System.out.println(main.maxDeepFile.getName());
+    }
+    public void findDeep(File file) {
+
+        for (File fl : file.listFiles()) {
+            if (!fl.isDirectory()) {
+                //System.out.println(fl.getName() + " not a directory");
+                //System.out.println(num + 1);
+                if (maxDeepNum < num + 1) {
+                    maxDeepNum = num + 1;
+                    maxDeepFile = fl;
+                }
+            } else if (fl.listFiles().length == 0) {
+                //System.out.println(fl.getName() + " is empty folder");
+                //System.out.println(num + 1);
+                if (maxDeepNum < num + 1) {
+                    maxDeepNum = num + 1;
+                    maxDeepFile = fl;
+                }
+            } else {
+                num++;
+                findDeep(fl);
+                //System.out.println(fl.getName() + " is a folder");
+                //System.out.println(num);
+                if (maxDeepNum < num + 1) {
+                    maxDeepNum = num + 1;
+                    maxDeepFile = fl;
+                }
+                num--;
+            }
+            numberOfFiles++;
+        }
+        //System.out.println(numberOfFiles);
+    }
+}
